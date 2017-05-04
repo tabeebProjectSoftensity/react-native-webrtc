11507187625429e6d145173f21d2976c3ae20403
Alexander Chernoshej <demoman361@gmail.com>
2017-05-04 12:44:08 +0300

    import deprecated Camera class

--- android/src/main/java/com/oney/WebRTCModule/CameraEventsHandler.java
+++ android/src/main/java/com/oney/WebRTCModule/CameraEventsHandler.java
@@ -3,6 +3,7 @@ package com.oney.WebRTCModule;
 import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.hardware.Camera;
 
 import org.webrtc.CameraVideoCapturer;
 
